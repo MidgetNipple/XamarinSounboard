d09383ca336a9cfae1eef3b4bb630a9e615e8e5d
MidgetNipple <timloozen99@gmail.com>
2017-12-06 11:51:51 +0100

    Add files via upload

--- MainActivity.cs
+++ MainActivity.cs
@@ -4,13 +4,15 @@ using Android.OS;
 using Android.Media;
 using System.Collections.Generic;
 using System.Threading;
+using System.IO;
 
 namespace App1
 {
+
     [Activity(Label = "SoundBoard", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        
+        private MediaPlayer mediaPlayer;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,107 +20,34 @@ namespace App1
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            addbutton();
 
-        }
-        MediaPlayer mediaPlayer;
-
-        public void sound(string sounds)
-        {
-
-            
-          
-           
-
-            switch (sounds)
+            DirectoryInfo di = new DirectoryInfo("//storage//emulated//0//SoundXamarin");
+            FileInfo[] fis = di.GetFiles("*.wav");
+            mediaPlayer = new MediaPlayer();
+            LinearLayout ll_main = FindViewById<LinearLayout>(Resource.Id.ll_main);
+            foreach (FileInfo fi in fis)
             {
-
-                case "lick lick my balls":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.lick_my_balls);
-                    mediaPlayer.Start();
-                         break;
-
-                case "aids":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.test);
-                    mediaPlayer.Start();
-                      break;
-
-                case "retarded":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.test2);
-                    mediaPlayer.Start();
-                    
-                    break;
-                case "dupdup":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.test3);
-                    mediaPlayer.Start();
-                    
-                    break;
-                case "tiny rick":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.rick);
-                    mediaPlayer.Start();
-                    
-                    break;
-                case "goodjob":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.Good_Job);
-                    mediaPlayer.Start();
-                    
-                    break;
-                case "aaaw bitch":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.Awww_Bitch);
-                    mediaPlayer.Start();
-                   
-                    break;
-                case "Ryan":
-                    mediaPlayer = MediaPlayer.Create(this, Resource.Raw.Ryan);
-                    mediaPlayer.Start();
-                   break;
-
-                default:
-                 
-                    break;
+                Button b = new Button(this.BaseContext);
+                b.Text = fi.Name;
+                b.Click += delegate
+                {
+                    lock (mediaPlayer)
+                    {
+                        mediaPlayer.Reset();
+                        mediaPlayer.SetDataSource(fi.FullName);
+                        mediaPlayer.Prepare();
+                        mediaPlayer.Start();
+                    }
+                };
+                ll_main.AddView(b);
             }
-            
-
-        }
-
-        public void addbutton()
-
-        {
-            
-
-            
-
-            List<string> buttonlist = new List<string>
-            {
-                "lick lick my balls",
-                "aids",
-                "retarded",
-                "dupdup",
-                "tiny rick",
-                "goodjob",
-                "aaaw bitch",
-                "Ryan",
-
-            };
-          
-            foreach (string button in buttonlist)
-            {
-                LinearLayout parentLayoutView = FindViewById<LinearLayout>(Resource.Id.LinearLayoutMain);
-                var aButton = new Button(this);
-                aButton.Click += (sender, e) =>
-                 { sound(button); };
-               
-                aButton.Text = button;
-                parentLayoutView.AddView(aButton);
-
-
-            }
-    
-
-
-
-
-
         }
     }
 }
+
+
+
+
+
+
+
